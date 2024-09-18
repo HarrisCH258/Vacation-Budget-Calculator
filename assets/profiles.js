760a82ad3b6b496de8d96b03d1418f8d5cdfcb30
Maverick S <maverickmmsc@gmail.com>
2024-09-18 17:58:44 -0400

    Profiles.html updated for new script

--- assets/profiles.js
+++ assets/profiles.js
@@ -1,3 +1,28 @@
+function readLocalStorage(key) {
+    const data = localStorage.getItem(key);
+    return data ? JSON.parse(data) : [];
+}
+  
+function storeLocalStorage(key, newData) {
+    const existingData = localStorage.getItem(key);
+    let dataArray = existingData ? JSON.parse(existingData) : [];
+  
+    dataArray.push(newData);
+  
+    localStorage.setItem(key, JSON.stringify(dataArray));
+}
+
+function displayNoProfileMessage(containerSelector) {
+    var container = document.querySelector(containerSelector);
+    
+    container.innerHTML = '';
+
+    var messageElement = document.createElement('p');
+    messageElement.textContent = "No profiles found. Please create a profile on the home page.";
+
+    container.appendChild(messageElement);
+}
+
 function calculateNetIncome() {
     let totalExpenses = mathfloor(rent.value + utilities.value + car.value + living.value);
     
